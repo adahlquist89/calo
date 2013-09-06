98a1004a5948b211143928d24f97a2adaaa06d48
frandorr <fran.dorr@gmail.com>
2013-09-06 10:13:02 -0300

    Perfiles con SocialAuth

--- calo_social/calo_social/profile/models.py
+++ calo_social/calo_social/profile/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 
 class Profile(models.Model):
     
-    user = models.OneToOneField(User, related_name="profile")
+    
+
+    """Main Profile Object - Holds extra profile data retrieved from auth providers"""
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    user = models.OneToOneField(User, related_name="profile", verbose_name=_("Profile"))
+    gender = models.CharField(max_length=1, blank=True, choices=GENDER_CHOICES, verbose_name=_("Gender"))
+    image_url = models.URLField(blank=True, verbose_name=_("Avatar Picture"))
+    description = models.TextField(blank=True, verbose_name=_("Description"), help_text=_("Tell us about yourself!"))
+
+    class Meta:
+        verbose_name = _("Profile")
+        verbose_name_plural = _("Profiles")
+        ordering = ['user__username']
+
+    def __unicode__(self):
+        return self.user.username
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'profile_other_view_page', [self.user.username]
+
 
--- calo_social/calo_social/profile/receivers.py
+++ calo_social/calo_social/profile/receivers.py
@@ -2,8 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
-
 from .models import Profile
+from social_auth.backends.google import GoogleOAuth2Backend
+from social_auth.signals import socialauth_registered, pre_update
 
 
 @receiver(post_save, sender=User)
@@ -12,3 +13,35 @@ def create_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
+
+
+
+@receiver(socialauth_registered, sender=GoogleOAuth2Backend)
+def google_extra_values(sender, user, response, details, **kwargs):
+    """Populates a UserProfile Object when a new User is created via Google Auth"""
+    LOGGER.debug('profile.models.google_extra_values')
+    user_info_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+
+    data = {'access_token': response.get('access_token', ''), 'alt': 'json'}
+    params = urlencode(data)
+    try:
+        request = Request(user_info_url + '?' + params, headers={'Authorization': params})
+        result = simplejson.loads(urlopen(request).read())
+
+        user.last_name = result.get('family_name', '')
+        user.first_name = result.get('given_name', '')
+        profile = user.profile
+        profile.gender = result.get('gender', '')
+        profile.image_url = result.get('picture', '')
+        profile.url = result.get('link', '')
+
+        profile.save()
+    except:
+        pass
+
+    return True
+
+socialauth_registered.connect(google_extra_values, sender=GoogleOAuth2Backend)
+
+
+
